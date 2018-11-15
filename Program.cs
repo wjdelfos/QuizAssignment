cc073c21292e5ffd6ab8d49e83f17d373fc24ad7
Unknown <willem@delfos.nu>
2018-11-15 11:31:58 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuizAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizAssignment
+{
+    class Question
+    {
+        protected string Text { get; set; }
+        protected string CorrectAnswer { get; set; }
+
+        public Question()
+        {
+            Text = "";
+            CorrectAnswer = "";
+        }
+
+
+        public Boolean CheckAnswer(string a) {
+            return a.Equals(CorrectAnswer);
+        }
+
+        public void Display() {
+            Console.WriteLine(Text);
+        }
+
+    }
+}
